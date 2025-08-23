dc2ffa54411b3b722b97969793a54d4800d270e3
Just_Ho_it <zalop3128@kakao.com>
2025-08-24 05:48:31 +0900

    [FEAT]Headline 컴포넌트

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,13 @@
 import { Component } from '../core/core'
+import Headline from '../components/HeadLine'
 
 export default class Home extends Component {
     render() {
-        this.el.innerHTML = /* html */ `
-        <h1>Hellow World</h1>
-        `
+        const headline = new Headline().el
+        
+        this.el.classList.add('container')
+        this.el.append(
+            headline
+        )
     }
 }

7930b7f15a6ca07110f64c9404c84c63cd410e45
Just_Ho_it <zalop3128@kakao.com>
2025-08-24 20:48:31 +0900

    [FEAT]영화 정보, 영화 리스트 받아오기

--- src/store/movie.js
+++ src/store/movie.js
@@ -6,13 +6,21 @@ const API_KEY = process.env.API_KEY;
 const store = new Store({
     searchText: '',
     page: 1,
+    pageMax: 1,
     movies: []
 })
 
-
 export default store
 export const searchMovies = async page => {
+    store.state.page = page
+    if (page === 1) {
+        store.state.movies = []
+    }
     const res = await fetch(`https://omdbapi.com?apikey=${API_KEY}&s=${store.state.searchText}&page=${page}`)
-    const json = await res.json()
-    console.log(json)
+    const { Search, totalResults } = await res.json()
+    store.state.movies = [
+        ...store.state.movies,
+        ...Search
+    ]
+    store.state.pageMax = Math.ceil(Number(totalResults) / 10)
 }

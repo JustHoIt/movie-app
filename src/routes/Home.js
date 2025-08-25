f99a9ddabca413b551fd0d4f37ea638b574986da
Just_Ho_it <zalop3128@kakao.com>
2025-08-26 04:19:12 +0900

    [FIX]import 불일치 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import { Component } from '../core/core'
 import Headline from '../components/Headline'
 import Search from '../components/Search'
-import MovieList from '../components/Movielist'
+import MovieList from '../components/MovieList'
 import MovieListMore from '../components/MovieListMore'
 
 export default class Home extends Component {
